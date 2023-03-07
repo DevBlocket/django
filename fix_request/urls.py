9de3760f35ba21a6695e7e018ff4a437c6aca454
re-meal <remeal.shop@gmail.com>
2023-03-07 15:45:23 +0900

    basename 추가

--- fix_request/urls.py
+++ fix_request/urls.py
@@ -4,7 +4,7 @@ from rest_framework import routers
 from fix_request.view_sets import FixRequestModelViewSet
 
 router = routers.SimpleRouter()
-router.register('', FixRequestModelViewSet)
+router.register('', FixRequestModelViewSet, basename='fix-request')
 
 urlpatterns = [
     path('', include(router.urls)),
