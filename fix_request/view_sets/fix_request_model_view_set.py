b1117f9898cb9d5d8b905d004238167807ef01cd
re-meal <remeal.shop@gmail.com>
2023-03-07 15:40:24 +0900

    user_id params 추가

--- fix_request/view_sets/fix_request_model_view_set.py
+++ fix_request/view_sets/fix_request_model_view_set.py
@@ -6,6 +6,14 @@ from fix_request.serializers import FixRequestSerializer
 
 class FixRequestModelViewSet(viewsets.ModelViewSet):
 
-    queryset = FixRequest.objects.all()
     serializer_class = FixRequestSerializer
+
+    def get_queryset(self):
+
+        queryset = FixRequest.objects.all()
+        user_id = self.request.query_params.get('user_id')
+        if user_id is not None:
+            queryset = queryset.filter(user_id=user_id)
+        return queryset
+
 
